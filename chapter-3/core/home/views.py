9505cc92ac1886b2021749d2c2359e6467490887
sahilsaiyed-oss <sahilsaiyed067@gmail.com>
2026-02-10 20:55:28 +0530

    Updated multiple Django files (settings, models, views)

--- chapter-3/core/home/views.py
+++ chapter-3/core/home/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Hey I am a Django Server.</h1>")
-
+   return render(request , "index.html")
 
 
 def success_page(request):
+    print("*" * 10)
+
     return HttpResponse("<h1> Hey this is a success page</h1>")
