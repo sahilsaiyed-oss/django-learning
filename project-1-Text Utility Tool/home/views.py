0c019882cb518b2265e887a8ab6ce25cc38bf3b9
sahilsaiyed-oss <sahilsaiyed067@gmail.com>
2026-02-17 22:28:10 +0530

    feat: added Title Case transformation feature

--- project-1-Text Utility Tool/home/views.py
+++ project-1-Text Utility Tool/home/views.py
@@ -6,9 +6,10 @@ def index(request):
 def analyze(request):
     djtext = request.POST.get('text', 'default')
     uppercase = request.POST.get('uppercase', 'off')
+    # NEW: Get Title Case checkbox status
+    titlecase = request.POST.get('titlecase', 'off')
     extraspace = request.POST.get('extraspace', 'off')
     charcount = request.POST.get('charcount', 'off')
-    # NEW: Get wordcount checkbox status
     wordcount = request.POST.get('wordcount', 'off')
 
     purpose = ""
@@ -18,6 +19,11 @@ def analyze(request):
         djtext = djtext.upper()
         purpose += "| Uppercase "
 
+    # NEW: Title Case Logic
+    if titlecase == "on":
+        djtext = djtext.title()
+        purpose += "| Title Case "
+
     if extraspace == "on":
         djtext = " ".join(djtext.split())
         purpose += "| Extra Spaces Removed "
@@ -26,18 +32,15 @@ def analyze(request):
         params['count'] = len(djtext)
         purpose += "| Character Counted "
 
-    # NEW: Word Count and Read Time Logic
     if wordcount == "on":
         words = len(djtext.split())
         params['words'] = words
-        # Average reading speed is 200 words per minute
         params['read_time'] = round(words / 200, 2)
         purpose += "| Text Statistics Analyzed "
 
     params['analyzed_text'] = djtext
     params['purpose'] = purpose
     return render(request, 'home/index.html', params)
-
 
 def custom_404(request, exception):
     return render(request, 'home/404.html', status=404)
